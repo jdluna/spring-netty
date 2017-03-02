a493945406cd9b01e580d52857b33053901ae38b
Sotheara Leang <leangsotheara@gmail.com>
2017-03-02 21:41:55 +0700

    Commit

--- src/main/java/com/example/netty/jmeter/ClientSample.java
+++ src/main/java/com/example/netty/jmeter/ClientSample.java
@@ -1,8 +1,5 @@
 package com.example.netty.jmeter;
 
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-
 import java.net.InetSocketAddress;
 
 import org.springframework.context.ApplicationContext;
@@ -10,6 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.example.netty.config.AppConfig;
 import com.example.netty.iso8583.MessageFactory;
+import com.example.netty.util.FutureAggregator;
 import com.example.netty.util.NettyClient;
 import com.solab.iso8583.IsoMessage;
 import com.solab.iso8583.IsoType;
@@ -17,24 +15,9 @@ import com.solab.iso8583.IsoValue;
 
 public class ClientSample {
 	
-	static int counter = 0;
-	static int msgNum;
-	
-	static ChannelFutureListener listener = new ChannelFutureListener() {
-		
-		@Override
-		public void operationComplete(ChannelFuture future) throws Exception {
-			counter++;
-		}
-	};
-	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws InterruptedException {
 		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
-		
-		msgNum = Integer.valueOf(args[0]);
-		
-		MessageFactory msgFacotry = appContext.getBean(MessageFactory.class);
 		
 		NettyClient nettyClient = appContext.getBean(NettyClient.class);
 		nettyClient.start();
@@ -43,16 +26,19 @@ public class ClientSample {
 		String host = remoteAddress.getHostName();
 		int port = remoteAddress.getPort();
 		
+		FutureAggregator<Void> futureAggregator = new FutureAggregator<>();
+		
+		MessageFactory msgFacotry = appContext.getBean(MessageFactory.class);
+		
+		int msgNum = Integer.valueOf(args[0]);
 		
 		for (int i = 0; i < msgNum; i++) {
 			IsoMessage message = msgFacotry.newMessage(0x200);
 			message.setField(32, new IsoValue<String>(IsoType.LLVAR, String.format("#%s:%d#_", host, port) + i));
 			
-			nettyClient.writeAndFlush(message).addListener(listener);
+			futureAggregator.addFuture(nettyClient.writeAndFlush(message));
 		}
-		
-		while (counter != msgNum) {
-			Thread.sleep(1000);
-		}
+	
+		futureAggregator.syn();
 	}
 }
