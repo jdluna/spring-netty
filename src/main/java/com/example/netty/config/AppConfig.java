11e15c158742ea62a6521fd6dd87f8e474f2fd87
Sotheara Leang <sotheara.leang@mcnc.co.kr>
2017-03-03 10:58:44 +0700

    Commit

--- src/main/java/com/example/netty/config/AppConfig.java
+++ src/main/java/com/example/netty/config/AppConfig.java
@@ -1,25 +1,36 @@
 package com.example.netty.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.Marshaller;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
-@ComponentScan(
-	basePackages = "com.example.netty", 
-	excludeFilters = {
-		@Filter(type = FilterType.REGEX, pattern = "com.example.netty.iso20022..*")
-	}
-)
+@ComponentScan("com.example.netty")
 @PropertySource("classpath:application.properties")
 public class AppConfig {
 	
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 	    return new PropertySourcesPlaceholderConfigurer();
+	}
+	
+	@Bean
+	public Jaxb2Marshaller jaxbMarshaller() {
+	  Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+	  jaxb2Marshaller.setPackagesToScan("com.example.netty.iso20022");
+	  
+	  Map<String, Object> map = new HashMap<String, Object>();
+	  map.put(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+	  
+	  jaxb2Marshaller.setMarshallerProperties(map);
+      return jaxb2Marshaller;
 	}
 }
