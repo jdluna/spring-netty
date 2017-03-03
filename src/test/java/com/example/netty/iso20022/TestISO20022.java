ec500ad94c5acf9c4631d7c3b92dd246bad237e1
Sotheara Leang <leangsotheara@gmail.com>
2017-03-03 21:32:51 +0700

    Commit

--- src/test/java/com/example/netty/iso20022/TestISO20022.java
+++ src/test/java/com/example/netty/iso20022/TestISO20022.java
@@ -3,9 +3,13 @@ package com.example.netty.iso20022;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
-import javax.xml.bind.JAXBElement;
-import javax.xml.namespace.QName;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
 import javax.xml.transform.stream.StreamResult;
 
 import org.junit.Test;
@@ -19,24 +23,42 @@ public class TestISO20022 extends AbstractTestCase {
 
 	@Autowired
 	private Jaxb2Marshaller marshaller;
-	
+
 	@Autowired
 	private ObjectFactory objectFactory;
-	
+
 	@Test
-	public void testEncode() throws XmlMappingException, IOException {
+	public void testEncode() throws XmlMappingException, IOException, DatatypeConfigurationException {
 		GroupHeader48 groupHeader48 = objectFactory.createGroupHeader48();
-		groupHeader48.setCtrlSum(new BigDecimal(200));
+		groupHeader48.setCtrlSum(new BigDecimal(1));
 		groupHeader48.setMsgId("0001223");
+		groupHeader48.nbOfTxs = "000121";
 		
-		CustomerCreditTransferInitiationV05 cstmrCdtTrfInitn = objectFactory.createCustomerCreditTransferInitiationV05();
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(new Date());
+		groupHeader48.creDtTm = DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
+		
+		Authorisation1Choice authChoice = new Authorisation1Choice();
+		authChoice.prtry = "001";
+		authChoice.cd = Authorisation1Code.ILEV;
+		
+		List<Authorisation1Choice> authstns = new ArrayList<>();
+		authstns.add(authChoice);
+		
+		groupHeader48.authstns = authstns;
+		
+		PartyIdentification43 partyId = new PartyIdentification43();
+		partyId.nm = "0232";
+		
+		groupHeader48.initgPty = partyId;
+	
+		CustomerCreditTransferInitiationV05 cstmrCdtTrfInitn = new CustomerCreditTransferInitiationV05();
 		cstmrCdtTrfInitn.setGrpHdr(groupHeader48);
 		
+
 		Document document = objectFactory.createDocument();
 		document.setCstmrCdtTrfInitn(cstmrCdtTrfInitn);
 		
-		Object object = new JAXBElement<Document>(new QName("uri","local"), Document.class, document);
-		
-		marshaller.marshal(object, new StreamResult(new FileWriter("schema.xml")));
+		marshaller.marshal(document, new StreamResult(new FileWriter("schema.xml")));
 	}
 }
