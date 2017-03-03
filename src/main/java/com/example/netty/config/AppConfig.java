bc49bc7f3037fc43681b57523c7f056014757b07
Sotheara Leang <sotheara.leang@mcnc.co.kr>
2017-03-03 13:16:33 +0700

    Commit

--- src/main/java/com/example/netty/config/AppConfig.java
+++ src/main/java/com/example/netty/config/AppConfig.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import javax.xml.bind.Marshaller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +19,9 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 @PropertySource("classpath:application.properties")
 public class AppConfig {
 	
+	@Autowired
+	private ApplicationContext appContext;
+	
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 	    return new PropertySourcesPlaceholderConfigurer();
@@ -24,13 +29,14 @@ public class AppConfig {
 	
 	@Bean
 	public Jaxb2Marshaller jaxbMarshaller() {
-	  Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-	  jaxb2Marshaller.setPackagesToScan("com.example.netty.iso20022");
+	  Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+	  marshaller.setPackagesToScan("com.example.netty.iso20022");
+	  marshaller.setSchema(appContext.getResource("classpath:schema/pain.001.001.05.xsd"));
 	  
 	  Map<String, Object> map = new HashMap<String, Object>();
 	  map.put(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 	  
-	  jaxb2Marshaller.setMarshallerProperties(map);
-      return jaxb2Marshaller;
+	  marshaller.setMarshallerProperties(map);
+      return marshaller;
 	}
 }
