f8734067d9f7e97cb0558d0cdb98f20bd606aa2e
Sotheara Leang <leangsotheara@gmail.com>
2017-03-16 22:04:54 +0700

    Commit

--- src/main/java/com/example/netty/base/iso8583/codec/ISO8583Encoder.java
+++ src/main/java/com/example/netty/base/iso8583/codec/ISO8583Encoder.java
@@ -2,18 +2,18 @@ package com.example.netty.base.iso8583.codec;
 
 import java.util.List;
 
+import com.example.netty.base.iso8583.ISO8583;
 import com.example.netty.base.iso8583.ISO8583BytesWrapper;
-import com.solab.iso8583.IsoMessage;
 
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
 
 @Sharable
-public class ISO8583Encoder extends MessageToMessageEncoder<IsoMessage> {
+public class ISO8583Encoder extends MessageToMessageEncoder<ISO8583> {
 
 	@Override
-	protected void encode(ChannelHandlerContext ctx, IsoMessage msg, List<Object> out) throws Exception {
+	protected void encode(ChannelHandlerContext ctx, ISO8583 msg, List<Object> out) throws Exception {
 		ISO8583BytesWrapper wrapper = new ISO8583BytesWrapper();
 		wrapper.setBytes(msg.writeData());
 		
