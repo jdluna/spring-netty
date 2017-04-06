fbf2b74d19e8a959fb706f2397d72a4afce15383
Sotheara Leang <leangsotheara@gmail.com>
2017-04-06 18:54:02 +0700

    Commit

--- src/test/java/com/example/netty/sample/conf/AppConfig.java
+++ src/test/java/com/example/netty/sample/conf/AppConfig.java
@@ -1,7 +1,5 @@
 package com.example.netty.sample.conf;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +10,6 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @ComponentScan("com.example.netty.sample")
 @PropertySource("classpath:application.properties")
 public class AppConfig {
-	
-	@Autowired
-	private ApplicationContext appContext;
 	
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
